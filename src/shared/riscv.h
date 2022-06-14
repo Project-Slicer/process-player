3fc526fda7a6654aa12a3040eb14f06f7eae180e
MaxXSoft <x@MaxXSoft.net>
2022-06-14 16:19:02 +0800

    (riscv) added more macros for assembler

--- src/shared/riscv.h
+++ src/shared/riscv.h
@@ -4,4 +4,22 @@
 #define RISCV_PGSHIFT 12
 #define RISCV_PGSIZE (1 << RISCV_PGSHIFT)
 
+#if __riscv_xlen == 64
+#define REGBYTES 8
+#define LOAD ld
+#else
+#define REGBYTES 4
+#define LOAD lw
+#endif
+
+#ifdef __riscv_flen
+#if __riscv_flen == 64
+#define FLOAD fld
+#elif __riscv_flen == 32
+#define FLOAD flw
+#else
+#error "Unsupported floating point length"
+#endif
+#endif
+
 #endif  // PP_SHARED_RISCV_H_
