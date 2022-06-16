787bbe208ed85d2b871ac88577768ecc453dd584
MaxXSoft <x@MaxXSoft.net>
2022-06-16 15:01:00 +0800

    (main) added implementations for loading post PP

--- src/pre/main.c
+++ src/pre/main.c
@@ -1,7 +1,9 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
 #include "pre/file.h"
@@ -11,6 +13,10 @@
 #include "shared/utils.h"
 
 static const char *checkpoint_dir;
+
+// post PP related stuffs
+#define POST_PP_STACK_SIZE 2048
+extern char post_pp_begin, post_pp_end;
 
 static void help(const char *progname) {
   printf("Process Player\n\n");
@@ -42,16 +48,20 @@ static bool check_platinfo() {
   close_assert(fd);
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-  const int endianess = 0;
+  const int endian = 0;
 #else
-  const int endianess = 1;
+  const int endian = 1;
 #endif
 
   if (platinfo.magic[0] != 'p' || platinfo.magic[1] != 'i') return false;
-  if (platinfo.endian != endianess) return false;
+  if (platinfo.endian != endian) return false;
   if (platinfo.ptr_size != sizeof(void *)) return false;
   if (platinfo.page_size != RISCV_PGSIZE) return false;
   return platinfo.major <= PLATINFO_MAJOR && platinfo.minor <= PLATINFO_MINOR;
+}
+
+static char *get_post_pp_base(size_t len) {
+  // TODO
 }
 
 int main(int argc, const char *argv[]) {
@@ -74,11 +84,20 @@ int main(int argc, const char *argv[]) {
   restore_fds();
 
   // scan for address space holes
-  // TODO
+  size_t bss_len = (size_t)(*((uint32_t *)&post_pp_end - 1));
+  size_t post_pp_len =
+      ROUNDUP(&post_pp_end - &post_pp_begin + bss_len + POST_PP_STACK_SIZE, 16);
+  char *post_pp_base = get_post_pp_base(post_pp_len);
 
-  // load the post PP and allocate stack
-  // TODO
+  // load the post PP and initialize
+  if (mmap(post_pp_base, post_pp_len, PROT_READ | PROT_WRITE | PROT_EXEC,
+           MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0) != post_pp_base) {
+    PANIC("failed to map the post PP");
+  }
+  memcpy(post_pp_base, &post_pp_begin, &post_pp_end - &post_pp_begin);
+  memset(post_pp_base + (&post_pp_end - &post_pp_begin), 0, bss_len);
 
   // call the post part of PP
-  // TODO: utils_post_init
+  uintptr_t post_pp_sp = (uintptr_t)(post_pp_base + post_pp_len);
+  utils_post_init(post_pp_sp, (uintptr_t)post_pp_base);
 }
