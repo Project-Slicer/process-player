4b45e0a83b16a0e668754e2cc604906d79319a6a
MaxXSoft <x@MaxXSoft.net>
2022-06-16 11:37:25 +0800

    (riscv) removed some redundant definitions

--- src/shared/riscv.h
+++ src/shared/riscv.h
@@ -4,14 +4,6 @@
 #define RISCV_PGSHIFT 12
 #define RISCV_PGSIZE (1 << RISCV_PGSHIFT)
 
-#if __riscv_xlen == 64
-#define REGBYTES 8
-#define LOAD ld
-#else
-#define REGBYTES 4
-#define LOAD lw
-#endif
-
 #ifdef __riscv_flen
 #if __riscv_flen == 64
 #define FLOAD fld
