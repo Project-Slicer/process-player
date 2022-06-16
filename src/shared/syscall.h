4d7df7af9491924ea7956b1d3cd917ce835a176e
MaxXSoft <x@MaxXSoft.net>
2022-06-16 11:57:21 +0800

    (syscall) added more details

--- src/shared/syscall.h
+++ src/shared/syscall.h
@@ -1,6 +1,10 @@
 #ifndef PP_SHARED_SYSCALL_H_
 #define PP_SHARED_SYSCALL_H_
 
-#define SYS_START 19980130
+// System call number of the custom system call `pp_start`.
+// `pp_start` will restore the trapframe and start the process.
+// Arguments:
+//   tf: trapframe_t*
+#define SYS_PP_START 19980130
 
 #endif  // PP_SHARED_SYSCALL_H_
