72605614bb207dbfb876437b8e7f568ba4a392b5
MaxXSoft <x@MaxXSoft.net>
2022-06-17 19:26:44 +0800

    (utils) updated some details about `PANIC_IF`

--- src/shared/utils.h
+++ src/shared/utils.h
@@ -35,17 +35,16 @@ int openr(const char *path);
 
 static inline int openr_assert(const char *path) {
   int fd = openr(path);
-  if (fd < 0) PANIC("failed to open %s", path);
+  PANIC_IF(fd < 0, "Failed to open %s", path);
   return fd;
 }
 
 static inline void read_assert(int fd, void *buf, size_t size) {
-  size_t nread = read(fd, buf, size);
-  if (nread != size) PANIC("failed to read from fd %d", fd);
+  PANIC_IF(read(fd, buf, size) != size, "failed to read from fd %d", fd);
 }
 
 static inline void close_assert(int fd) {
-  if (close(fd) < 0) PANIC("failed to close fd %d", fd);
+  PANIC_IF(close(fd) < 0, "failed to close fd %d", fd);
 }
 
 #endif  // PP_SHARED_UTILS_H_
